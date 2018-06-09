2d046c91050faa437cd1844d0a2bd779697dff5e
belger-dussen <belger.dussen@gmail.com>
2018-06-10 02:56:22 +0900

    auto-pep8

--- yolo-v3/loss.py
+++ yolo-v3/loss.py
@@ -14,20 +14,20 @@ def build_targets(pred_boxes, target, anchors, num_anchors, num_classes, nH, nW,
     nA = num_anchors
     nC = num_classes
     # sually anchors is pair of integer, so anchor_step is usually 2.
-    anchor_step = len(anchors)//num_anchors
+    anchor_step = len(anchors) // num_anchors
     # confusion mask, if no obj exist,  5
-    conf_mask = torch.ones(nB, nA*nH*nW).to(device) * noobject_scale
-    coord_mask = torch.zeros(nB, nA*nH*nW).to(device)
-    cls_mask = torch.zeros(nB, nA*nH*nW).to(device)
-    tx = torch.zeros(nB, nA*nH*nW).to(device)
-    ty = torch.zeros(nB, nA*nH*nW).to(device)
-    tw = torch.zeros(nB, nA*nH*nW).to(device)
-    th = torch.zeros(nB, nA*nH*nW).to(device)
-    tconf = torch.zeros(nB, nA*nH*nW).to(device)
-    tcls = torch.zeros(nB, nA*nH*nW, nC).to(device)
+    conf_mask = torch.ones(nB, nA * nH * nW).to(device) * noobject_scale
+    coord_mask = torch.zeros(nB, nA * nH * nW).to(device)
+    cls_mask = torch.zeros(nB, nA * nH * nW).to(device)
+    tx = torch.zeros(nB, nA * nH * nW).to(device)
+    ty = torch.zeros(nB, nA * nH * nW).to(device)
+    tw = torch.zeros(nB, nA * nH * nW).to(device)
+    th = torch.zeros(nB, nA * nH * nW).to(device)
+    tconf = torch.zeros(nB, nA * nH * nW).to(device)
+    tcls = torch.zeros(nB, nA * nH * nW, nC).to(device)
 
-    nAnchors = nA*nH*nW
-    nPixels = nH*nW
+    nAnchors = nA * nH * nW
+    nPixels = nH * nW
     for b in range(nB):
         cur_pred_boxes = pred_boxes[b, 0:nAnchors]
         cur_ious = torch.zeros(nAnchors).to(device)
@@ -36,13 +36,13 @@ def build_targets(pred_boxes, target, anchors, num_anchors, num_classes, nH, nW,
         #  cl : class number in the definition of the config file whose name is like "*.name".
         #  tx, ty, tw, th: bounding box center, width and height, which are normalized by the image size.
         for t in range(50):
-            if target[b][t*5+1] == 0:
+            if target[b][t * 5 + 1] == 0:
                 break
-            gx = target[b][t*5+1]*nW
+            gx = target[b][t * 5 + 1] * nW
             # print(gx,cur_pred_boxes)
-            gy = target[b][t*5+2]*nH
-            gw = target[b][t*5+3]*nW
-            gh = target[b][t*5+4]*nH
+            gy = target[b][t * 5 + 2] * nH
+            gw = target[b][t * 5 + 3] * nW
+            gh = target[b][t * 5 + 4] * nH
             cur_gt_boxes = torch.FloatTensor(
                 [gx, gy, gw, gh]).repeat(nAnchors, 1).to(device)
             # print(cur_gt_boxes)
@@ -67,29 +67,29 @@ def build_targets(pred_boxes, target, anchors, num_anchors, num_classes, nH, nW,
     nCorrect = 0
     for b in range(nB):
         for t in range(50):
-            if target[b][t*5+1] == 0:
+            if target[b][t * 5 + 1] == 0:
                 break
             nGT = nGT + 1
             best_iou = 0.0
             best_n = -1  # best_anchor_number
             min_dist = 10000  # min distance
-            gx = target[b][t*5+1] * nW
-            gy = target[b][t*5+2] * nH
+            gx = target[b][t * 5 + 1] * nW
+            gy = target[b][t * 5 + 2] * nH
             gi = int(gx)    # (grid_index)x(img_size/stride)
             gj = int(gy)    #
-            gw = target[b][t*5+3] * nW
-            gh = target[b][t*5+4] * nH
+            gw = target[b][t * 5 + 3] * nW
+            gh = target[b][t * 5 + 4] * nH
             gt_box = torch.FloatTensor([0, 0, gw, gh]).view(1, 4).to(device)
             for n in range(nA):
-                aw = anchors[anchor_step*n]
-                ah = anchors[anchor_step*n + 1]
+                aw = anchors[anchor_step * n]
+                ah = anchors[anchor_step * n + 1]
                 anchor_box = torch.FloatTensor(
                     [0, 0, aw, ah]).view(1, 4).to(device)
                 iou = bbox_iou(anchor_box, gt_box, xywh=True)
                 if anchor_step == 4:
-                    ax = anchors[anchor_step*n + 2]
-                    ay = anchors[anchor_step*n + 3]
-                    dist = pow(((gi+ax) - gx), 2) + pow(((gj+ay) - gy), 2)
+                    ax = anchors[anchor_step * n + 2]
+                    ay = anchors[anchor_step * n + 3]
+                    dist = pow(((gi + ax) - gx), 2) + pow(((gj + ay) - gy), 2)
                 if iou > best_iou:
                     best_iou = iou
                     best_n = n
@@ -99,19 +99,19 @@ def build_targets(pred_boxes, target, anchors, num_anchors, num_classes, nH, nW,
                     min_dist = dist
 
             gt_box = torch.FloatTensor([gx, gy, gw, gh]).view(1, 4).to(device)
-            best_idx = nA*(nW*gi + gj)+best_n
+            best_idx = nA * (nW * gi + gj) + best_n
             pred_box = pred_boxes[b, best_idx].view(1, 4)
-            tcl_idx = int(target[b][t*5])
+            tcl_idx = int(target[b][t * 5])
 
             coord_mask[b, best_idx] = 1
             cls_mask[b, best_idx] = 1
             conf_mask[b, best_idx] = object_scale
 
             # (tx, ty, tw, th) == (bx, by, bw, bh) in the paper of yolo-v3
-            tx[b, best_idx] = target[b][t*5+1]
-            ty[b, best_idx] = target[b][t*5+2]
-            tw[b, best_idx] = target[b][t*5+3]
-            th[b, best_idx] = target[b][t*5+4]
+            tx[b, best_idx] = target[b][t * 5 + 1]
+            ty[b, best_idx] = target[b][t * 5 + 2]
+            tw[b, best_idx] = target[b][t * 5 + 3]
+            th[b, best_idx] = target[b][t * 5 + 4]
             #tx[b, best_idx] = target[b][t*5+1] * nW - gi
             #ty[b, best_idx] = target[b][t*5+2] * nH - gj
             # tw[b, best_idx] = math.log(gw/anchors[anchor_step*best_n])  # ???
@@ -156,7 +156,7 @@ class V3Loss(nn.Module):
         for i, stride in enumerate(self.strides):
             nH = self.reso // stride
             nW = self.reso // stride
-            nAnchors = nH*nW*nA
+            nAnchors = nH * nW * nA
             anchors = self.anchors[i]
 
             # Normarize
@@ -178,18 +178,19 @@ class V3Loss(nn.Module):
                 "noobject_scale": 5, "object_scale": 1, "thresh": 0.6, "seen": False, "device": self.device
             }
             nGT, nCorrect, coord_mask, conf_mask, cls_mask, tx, ty, tw, th, tconf, tcls = build_targets(
-                vals["output"], vals["target"], vals["anchors"], vals["nA"], vals["nC"], vals["nH"], vals["nW"], vals["noobject_scale"], vals["object_scale"], vals["thresh"], vals["seen"], vals["device"])
+                vals["output"], vals["target"], vals["anchors"], vals["nA"], vals["nC"], vals["nH"], vals["nW"],
+                vals["noobject_scale"], vals["object_scale"], vals["thresh"], vals["seen"], vals["device"])
 
             loss_x = self.coord_scale * nn.MSELoss(size_average=False)(
-                scale_split_output[:, :, 0]*coord_mask, tx*coord_mask)/2.0
+                scale_split_output[:, :, 0] * coord_mask, tx * coord_mask) / 2.0
             loss_y = self.coord_scale * nn.MSELoss(size_average=False)(
-                scale_split_output[:, :, 1]*coord_mask, ty*coord_mask)/2.0
+                scale_split_output[:, :, 1] * coord_mask, ty * coord_mask) / 2.0
             loss_w = self.coord_scale * nn.MSELoss(size_average=False)(
-                scale_split_output[:, :, 2]*coord_mask, tw*coord_mask)/2.0
+                scale_split_output[:, :, 2] * coord_mask, tw * coord_mask) / 2.0
             loss_h = self.coord_scale * nn.MSELoss(size_average=False)(
-                scale_split_output[:, :, 3]*coord_mask, th*coord_mask)/2.0
+                scale_split_output[:, :, 3] * coord_mask, th * coord_mask) / 2.0
             loss_conf = nn.MSELoss(size_average=False)(
-                conf*conf_mask, tconf*conf_mask)/2.0
+                conf * conf_mask, tconf * conf_mask) / 2.0
             loss_cls = self.class_scale * \
                 nn.BCELoss(size_average=False)(cls, tcls)   # Todo
             loss = loss_x + loss_y + loss_w + loss_h + loss_conf + loss_cls
@@ -225,8 +226,8 @@ if __name__ == "__main__":
     scales_indices = []
     args.reso = int(args.reso)
     strides = [8, 16, 32]
-    resos = [args.reso//x for x in strides]
-    num_boxes = sum([3*(x**2) for x in resos])
+    resos = [args.reso // x for x in strides]
+    num_boxes = sum([3 * (x**2) for x in resos])
 
     for scale in scales:
         li = list(range((scale - 1) * num_boxes // 3, scale * num_boxes // 3))
@@ -247,7 +248,7 @@ if __name__ == "__main__":
         inp_dim = args.reso
         _output = torch.FloatTensor(np.ones((1, num_boxes, 85)))
 
-    _test_target = np.zeros((1, 5*50))
+    _test_target = np.zeros((1, 5 * 50))
     _test_target[:, 0] = 1
     _test_target[:, 1:5] = 0.2
     _test_target[:, 5] = 5
@@ -258,13 +259,13 @@ if __name__ == "__main__":
     #lossval = loss(test_output,test_target)
 
     # use prediction when stride 32
-    nH = args.reso//32
-    nW = args.reso//32
+    nH = args.reso // 32
+    nW = args.reso // 32
     nA = len(anchors) // 2
     nC = 80
 
     if False:
-        test_output = _output[:, 0:nH*nW*nA, 0:4]
+        test_output = _output[:, 0:nH * nW * nA, 0:4]
         vals = {
             "output": test_output,
             "target": test_target,
