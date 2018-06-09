b7048b6ee94549b77b4a92c458d6d0d06f43c950
belger-dussen <belger.dussen@gmail.com>
2018-06-09 20:00:09 +0900

    auto-pep8

--- yolo-v3/dataset.py
+++ yolo-v3/dataset.py
@@ -14,23 +14,24 @@ from scipy.misc import imread, imsave
 # from random import *
 import cv2
 
+
 class listDataset(Dataset):
 
     def __init__(self, root, shape=None, shuffle=True, transform=None, target_transform=None, train=False, seen=0, batch_size=64, num_workers=4):
-       with open(root, 'r') as file:
-           self.lines = file.readlines()
+        with open(root, 'r') as file:
+            self.lines = file.readlines()
 
-       if shuffle:
-           random.shuffle(self.lines)
+        if shuffle:
+            random.shuffle(self.lines)
 
-       self.nSamples  = len(self.lines)
-       self.transform = transform
-       self.target_transform = target_transform
-       self.train = train
-       self.shape = shape
-       self.seen = seen
-       self.batch_size = batch_size
-       self.num_workers = num_workers
+        self.nSamples = len(self.lines)
+        self.transform = transform
+        self.target_transform = target_transform
+        self.train = train
+        self.shape = shape
+        self.seen = seen
+        self.batch_size = batch_size
+        self.num_workers = num_workers
 
     def __len__(self):
         return self.nSamples
@@ -39,46 +40,49 @@ class listDataset(Dataset):
         assert index <= len(self), 'index range error'
         imgpath = self.lines[index].rstrip()
 
-        if self.train and index % 64== 0:
+        if self.train and index % 64 == 0:
             if self.seen < 4000*64:
-               width = 13*32
-               self.shape = (width, width)
+                width = 13*32
+                self.shape = (width, width)
             elif self.seen < 8000*64:
-               width = (random.randint(0,3) + 13)*32
-               self.shape = (width, width)
+                width = (random.randint(0, 3) + 13)*32
+                self.shape = (width, width)
             elif self.seen < 12000*64:
-               width = (random.randint(0,5) + 12)*32
-               self.shape = (width, width)
+                width = (random.randint(0, 5) + 12)*32
+                self.shape = (width, width)
             elif self.seen < 16000*64:
-               width = (random.randint(0,7) + 11)*32
-               self.shape = (width, width)
-            else: # self.seen < 20000*64:
-               width = (random.randint(0,9) + 10)*32
-               self.shape = (width, width)
+                width = (random.randint(0, 7) + 11)*32
+                self.shape = (width, width)
+            else:  # self.seen < 20000*64:
+                width = (random.randint(0, 9) + 10)*32
+                self.shape = (width, width)
 
         if self.train:
             jitter = 0.2
             hue = 0.1
-            saturation = 1.5 
+            saturation = 1.5
             exposure = 1.5
 
-            img, label = load_data_detection(imgpath, self.shape, jitter, hue, saturation, exposure)
+            img, label = load_data_detection(
+                imgpath, self.shape, jitter, hue, saturation, exposure)
             label = torch.from_numpy(label.astype('float32'))
-            
+
         else:
             img = Image.open(imgpath).convert('RGB')
             if self.shape:
                 img = img.resize(self.shape)
-    
-            labpath = imgpath.replace('images', 'labels').replace('JPEGImages', 'labels').replace('.jpg', '.txt').replace('.png','.txt')
+
+            labpath = imgpath.replace('images', 'labels').replace(
+                'JPEGImages', 'labels').replace('.jpg', '.txt').replace('.png', '.txt')
             label = torch.zeros(50*5)
 
-            #if os.path.getsize(labpath):
+            # if os.path.getsize(labpath):
             #tmp = torch.from_numpy(np.loadtxt(labpath))
             try:
-                tmp = torch.from_numpy(read_truths_args(labpath, 8.0/img.width).astype('float32'))
+                tmp = torch.from_numpy(read_truths_args(
+                    labpath, 8.0/img.width).astype('float32'))
             except Exception:
-                tmp = torch.zeros(1,5)
+                tmp = torch.zeros(1, 5)
             #tmp = torch.from_numpy(read_truths(labpath))
             tmp = tmp.view(-1)
             tsz = tmp.numel()
@@ -97,91 +101,101 @@ class listDataset(Dataset):
         self.seen = self.seen + self.num_workers
         return (img, label)
 
+
 class DataGenerator():
-    def __init__(self, batch_size, image_size, image_list, image_classid_list, max_num = 1, epoch_len=1000):
+    def __init__(self, batch_size, image_size, image_list, image_classid_list, max_num=1, epoch_len=1000):
         self.batch_size = batch_size
         self.image_size = image_size
         self.image_channel = image_list[0].shape[2]
-        
+
         self.image_list = image_list
         self.image_classid_list = image_classid_list
-        
+
         self.max_num = max_num
         self.epoch_len = epoch_len
-        
+
     def generate_image(self):
         idx = np.random.randint(0, len(self.image_list))
-        
+
         src_image = self.image_list[idx]
-        
+
         class_id = idx
-        
+
         # decide center
         bx = np.random.random()
         by = np.random.random()
-       
-        center = [(int) (bx*self.image_size[0]),(int) (by*self.image_size[1])]
-        
+
+        center = [(int)(bx*self.image_size[0]), (int)(by*self.image_size[1])]
+
         # decide scale
         scale = 0.4
         bw = src_image.shape[1]*scale/self.image_size[0]
         bh = src_image.shape[0]*scale/self.image_size[1]
 
-        resized_image = cv2.resize(src_image, ((int)(scale * src_image.shape[1]),(int)(scale * src_image.shape[0])))
-        
-        canvas = np.zeros((self.image_size[0], self.image_size[1]), dtype = np.uint8)
+        resized_image = cv2.resize(src_image, ((int)(
+            scale * src_image.shape[1]), (int)(scale * src_image.shape[0])))
 
-        T = np.array([[1.0, 0.0, 1.0*(int)(bx*canvas.shape[0]) - resized_image.shape[1]/2], 
-                                [0.0, 1.0, 1.0*(int)(by*canvas.shape[1]) - resized_image.shape[0]/2],
-                                [0.0, 0.0, 1.0]])
-        canvas = cv2.warpPerspective(resized_image, T, (canvas.shape[0],canvas.shape[1]))
-        
+        canvas = np.zeros(
+            (self.image_size[0], self.image_size[1]), dtype=np.uint8)
+
+        T = np.array([[1.0, 0.0, 1.0*(int)(bx*canvas.shape[0]) - resized_image.shape[1]/2],
+                      [0.0, 1.0, 1.0*(int)(by*canvas.shape[1]) -
+                       resized_image.shape[0]/2],
+                      [0.0, 0.0, 1.0]])
+        canvas = cv2.warpPerspective(
+            resized_image, T, (canvas.shape[0], canvas.shape[1]))
+
         return canvas, class_id, bx, by, bw, bh
 
     def generate(self):
         while True:
-            X = np.zeros((self.batch_size, self.image_channel, self.image_size[0], self.image_size[1]))
-            Y =  np.zeros((self.batch_size, 5*50))
+            X = np.zeros((self.batch_size, self.image_channel,
+                          self.image_size[0], self.image_size[1]))
+            Y = np.zeros((self.batch_size, 5*50))
             for i in range(self.batch_size):
                 img, cls, bx, by, bw, bh = self.generate_image()
-                X[i] = img.transpose(2,0,1)
-                #Y[i] = np.c_[cls, bx, by, bw, bh]  # np.r_かも
-                Y[i,:5] = np.array([cls, bx, by, bw, bh])  # np.r_かも
+                X[i] = img.transpose(2, 0, 1)
+                # Y[i] = np.c_[cls, bx, by, bw, bh]  # np.r_かも
+                Y[i, :5] = np.array([cls, bx, by, bw, bh])  # np.r_かも
             yield X, Y
-            
+
     def __len__(self):
         return self.epoch_len
+
 
 if __name__ == "__main__":
     image_data_path = "/ceph/rmurase/Datasets/card_templates/"
     _image_files = os.listdir(image_data_path)
-    image_file_names = list(map(lambda image_file_name : image_data_path + image_file_name , _image_files))
+    image_file_names = list(
+        map(lambda image_file_name: image_data_path + image_file_name, _image_files))
     print(image_file_names)
 
     image_files = []
 
     for n in image_file_names:
-            image_files.append(cv2.imread( n,1))
+        image_files.append(cv2.imread(n, 1))
     print(image_files[0])
-    dataloader = DataGenerator(2, [512, 512], image_files, image_file_names, max_num = 1)
-    print("len(dataloader)",len(dataloader))
+    dataloader = DataGenerator(
+        2, [512, 512], image_files, image_file_names, max_num=1)
+    print("len(dataloader)", len(dataloader))
     for iter, (input, target) in enumerate(dataloader.generate()):
         if iter == 3:
             print(input.shape, target.shape)
-            src = input[0].transpose(1,2,0)
+            src = input[0].transpose(1, 2, 0)
             print(src)
-            cls,bx,by,bw,bh = target[0][:5]
+            cls, bx, by, bw, bh = target[0][:5]
             im_size = src.shape[0]
             import matplotlib.patches as patches
             import matplotlib.pyplot as plt
-            bb = np.array([bx,by,bw,bh]).astype(np.int)
-            
+            bb = np.array([bx, by, bw, bh]).astype(np.int)
+
             # Create figure and axes
-            fig,ax = plt.subplots(1)
+            fig, ax = plt.subplots(1)
 
             # Display the image
             ax.imshow(src)
-            rect = patches.Rectangle((bb[0]-bb[2]/2,bb[1]-bb[3]/2),bb[2], bb[3], linewidth=3,edgecolor='r')
+            rect = patches.Rectangle(
+                (bb[0]-bb[2]/2, bb[1]-bb[3]/2), bb[2], bb[3], linewidth=3, edgecolor='r')
             ax.add_patch(rect)
             plt.savefig("bb_test.jpg")
 
@@ -190,9 +204,8 @@ if __name__ == "__main__":
             #t_l = [int(x) for x in t_l]
             #b_r = [int(x) for x in b_r]
             #cv2.rectangle(src, t_l, b_r, (0,255,0),3)
-            
-            
-            cv2.imwrite("test0.jpg", input[0].transpose(1,2,0))
-            cv2.imwrite("test1.jpg", input[1].transpose(1,2,0))
+
+            cv2.imwrite("test0.jpg", input[0].transpose(1, 2, 0))
+            cv2.imwrite("test1.jpg", input[1].transpose(1, 2, 0))
             break
     #data_loader = DataLoader(dataset=dataset, batch_size=1, shuffle=True)
--- yolo-v3/loss.py
+++ yolo-v3/loss.py
@@ -8,47 +8,54 @@ from util import *
 from bbox import *
 import argparse
 
+
 def build_targets(pred_boxes, target, anchors, num_anchors, num_classes, nH, nW, noobject_scale, object_scale, sil_thresh, seen, device):
-    nB = target.size(0) # num of batchess 
-    nA = num_anchors 
+    nB = target.size(0)  # num of batchess
+    nA = num_anchors
     nC = num_classes
-    anchor_step = len(anchors)//num_anchors # sually anchors is pair of integer, so anchor_step is usually 2. 
-    conf_mask  = torch.ones(nB, nA*nH*nW).to(device) * noobject_scale #confusion mask, if no obj exist,  5
+    # sually anchors is pair of integer, so anchor_step is usually 2.
+    anchor_step = len(anchors)//num_anchors
+    # confusion mask, if no obj exist,  5
+    conf_mask = torch.ones(nB, nA*nH*nW).to(device) * noobject_scale
     coord_mask = torch.zeros(nB, nA*nH*nW).to(device)
-    cls_mask   = torch.zeros(nB, nA*nH*nW).to(device)
-    tx         = torch.zeros(nB, nA*nH*nW).to(device)
-    ty         = torch.zeros(nB, nA*nH*nW).to(device) 
-    tw         = torch.zeros(nB, nA*nH*nW).to(device) 
-    th         = torch.zeros(nB, nA*nH*nW).to(device) 
-    tconf      = torch.zeros(nB, nA*nH*nW).to(device)
-    tcls       = torch.zeros(nB, nA*nH*nW, nC).to(device) 
+    cls_mask = torch.zeros(nB, nA*nH*nW).to(device)
+    tx = torch.zeros(nB, nA*nH*nW).to(device)
+    ty = torch.zeros(nB, nA*nH*nW).to(device)
+    tw = torch.zeros(nB, nA*nH*nW).to(device)
+    th = torch.zeros(nB, nA*nH*nW).to(device)
+    tconf = torch.zeros(nB, nA*nH*nW).to(device)
+    tcls = torch.zeros(nB, nA*nH*nW, nC).to(device)
 
     nAnchors = nA*nH*nW
-    nPixels  = nH*nW
+    nPixels = nH*nW
     for b in range(nB):
-        cur_pred_boxes = pred_boxes[b,0:nAnchors]
+        cur_pred_boxes = pred_boxes[b, 0:nAnchors]
         cur_ious = torch.zeros(nAnchors).to(device)
 
         #  If target image has n objects, target vector is like [cl0, tx0, ty0, tw0, th0, cl1, tx1, ty1, ...... , cln, txn, tyn, twn, thn, 0, 0, 0, 0, 0, 0, 0, .....]  (shape is [50])
-        #  cl : class number in the definition of the config file whose name is like "*.name".  
+        #  cl : class number in the definition of the config file whose name is like "*.name".
         #  tx, ty, tw, th: bounding box center, width and height, which are normalized by the image size.
-        for t in range(50):  
+        for t in range(50):
             if target[b][t*5+1] == 0:
                 break
             gx = target[b][t*5+1]*nW
-            #print(gx,cur_pred_boxes)
+            # print(gx,cur_pred_boxes)
             gy = target[b][t*5+2]*nH
             gw = target[b][t*5+3]*nW
             gh = target[b][t*5+4]*nH
-            cur_gt_boxes = torch.FloatTensor([gx,gy,gw,gh]).repeat(nAnchors,1).to(device)
-            #print(cur_gt_boxes)
-            cur_ious = torch.max(cur_ious, bbox_iou(cur_pred_boxes, cur_gt_boxes ,xywh=True))
+            cur_gt_boxes = torch.FloatTensor(
+                [gx, gy, gw, gh]).repeat(nAnchors, 1).to(device)
+            # print(cur_gt_boxes)
+            cur_ious = torch.max(cur_ious, bbox_iou(
+                cur_pred_boxes, cur_gt_boxes, xywh=True))
         #print("cur_ious",cur_ious.shape, "conf_mask", conf_mask.shape)
-        conf_mask[b][cur_ious>sil_thresh] = 0
+        conf_mask[b][cur_ious > sil_thresh] = 0
     if seen < 12800:
         if anchor_step == 4:
-            tx = torch.FloatTensor(anchors).view(nA, anchor_step).index_select(1, torch.LongTensor([2])).view(1,nA,1,1).repeat(nB,1,nH,nW).to(device)
-            ty = torch.FloatTensor(anchors).view(num_anchors, anchor_step).index_select(1, torch.LongTensor([2])).view(1,nA,1,1).repeat(nB,1,nH,nW).to(device)
+            tx = torch.FloatTensor(anchors).view(nA, anchor_step).index_select(
+                1, torch.LongTensor([2])).view(1, nA, 1, 1).repeat(nB, 1, nH, nW).to(device)
+            ty = torch.FloatTensor(anchors).view(num_anchors, anchor_step).index_select(
+                1, torch.LongTensor([2])).view(1, nA, 1, 1).repeat(nB, 1, nH, nW).to(device)
         else:
             tx.fill_(0.5)
             ty.fill_(0.5)
@@ -60,23 +67,24 @@ def build_targets(pred_boxes, target, anchors, num_anchors, num_classes, nH, nW,
     nCorrect = 0
     for b in range(nB):
         for t in range(50):
-            if target[b][t*5+1]==0:
+            if target[b][t*5+1] == 0:
                 break
             nGT = nGT + 1
             best_iou = 0.0
             best_n = -1  # best_anchor_number
-            min_dist = 10000 # min distance
+            min_dist = 10000  # min distance
             gx = target[b][t*5+1] * nW
             gy = target[b][t*5+2] * nH
-            gi = int(gx)    # (grid_index)x(img_size/stride) 
+            gi = int(gx)    # (grid_index)x(img_size/stride)
             gj = int(gy)    #
             gw = target[b][t*5+3] * nW
             gh = target[b][t*5+4] * nH
-            gt_box = torch.FloatTensor([0,0,gw,gh]).view(1,4).to(device)
+            gt_box = torch.FloatTensor([0, 0, gw, gh]).view(1, 4).to(device)
             for n in range(nA):
                 aw = anchors[anchor_step*n]
                 ah = anchors[anchor_step*n + 1]
-                anchor_box = torch.FloatTensor([0,0, aw, ah]).view(1,4).to(device)
+                anchor_box = torch.FloatTensor(
+                    [0, 0, aw, ah]).view(1, 4).to(device)
                 iou = bbox_iou(anchor_box, gt_box, xywh=True)
                 if anchor_step == 4:
                     ax = anchors[anchor_step*n + 2]
@@ -90,9 +98,9 @@ def build_targets(pred_boxes, target, anchors, num_anchors, num_classes, nH, nW,
                     best_n = n
                     min_dist = dist
 
-            gt_box = torch.FloatTensor([gx,gy,gw,gh]).view(1,4).to(device)
+            gt_box = torch.FloatTensor([gx, gy, gw, gh]).view(1, 4).to(device)
             best_idx = nA*(nW*gi + gj)+best_n
-            pred_box = pred_boxes[b, best_idx].view(1,4)
+            pred_box = pred_boxes[b, best_idx].view(1, 4)
             tcl_idx = int(target[b][t*5])
 
             coord_mask[b, best_idx] = 1
@@ -106,9 +114,9 @@ def build_targets(pred_boxes, target, anchors, num_anchors, num_classes, nH, nW,
             th[b, best_idx] = target[b][t*5+4]
             #tx[b, best_idx] = target[b][t*5+1] * nW - gi
             #ty[b, best_idx] = target[b][t*5+2] * nH - gj
-            #tw[b, best_idx] = math.log(gw/anchors[anchor_step*best_n])  # ???
-            #th[b, best_idx] = math.log(gh/anchors[anchor_step*best_n+1]) # ???
-            iou = bbox_iou(gt_box, pred_box) # best_iou
+            # tw[b, best_idx] = math.log(gw/anchors[anchor_step*best_n])  # ???
+            # th[b, best_idx] = math.log(gh/anchors[anchor_step*best_n+1]) # ???
+            iou = bbox_iou(gt_box, pred_box)  # best_iou
             tconf[b, best_idx] = iou
             tcls[b, best_idx, tcl_idx] = torch.FloatTensor([1]).to(device)
             if iou > 0.5:
@@ -142,123 +150,136 @@ class V3Loss(nn.Module):
         nB = output.data.size(0)
         nA = self.num_anchors
         nC = self.num_classes
-        self.output_split  = 0
+        self.output_split = 0
 
         for i, stride in enumerate(self.strides):
             nH = self.reso // stride
-            nW = self.reso // stride  
+            nW = self.reso // stride
             nAnchors = nH*nW*nA
             anchors = self.anchors[i]
 
-            scale_split_output = output[:, self.output_split : self.output_split + nAnchors, 0:4] / self.reso  # Normarize 
-            conf = output[:, self.output_split : self.output_split + nAnchors, 5]
-            cls = output[:, self.output_split : self.output_split + nAnchors, 5:]
+            # Normarize
+            scale_split_output = output[:,
+                                        self.output_split: self.output_split + nAnchors, 0:4] / self.reso
+            conf = output[:,
+                          self.output_split: self.output_split + nAnchors, 5]
+            cls = output[:, self.output_split: self.output_split + nAnchors, 5:]
             self.output_split = self.output_split + nAnchors
 
             vals = {
-                "output":scale_split_output,
-                "target":target,
-                "anchors":anchors,
-                "nA":nA,
-                "nC":nC,
-                "nH":nH,
-                "nW":nW,
-                "noobject_scale":5,"object_scale":1,"thresh":0.6, "seen":False, "device": self.device
+                "output": scale_split_output,
+                "target": target,
+                "anchors": anchors,
+                "nA": nA,
+                "nC": nC,
+                "nH": nH,
+                "nW": nW,
+                "noobject_scale": 5, "object_scale": 1, "thresh": 0.6, "seen": False, "device": self.device
             }
-            nGT, nCorrect, coord_mask, conf_mask, cls_mask, tx, ty, tw, th, tconf,tcls = build_targets(vals["output"],vals["target"],vals["anchors"],vals["nA"],vals["nC"],vals["nH"],vals["nW"],vals["noobject_scale"],vals["object_scale"],vals["thresh"],vals["seen"],vals["device"])
+            nGT, nCorrect, coord_mask, conf_mask, cls_mask, tx, ty, tw, th, tconf, tcls = build_targets(
+                vals["output"], vals["target"], vals["anchors"], vals["nA"], vals["nC"], vals["nH"], vals["nW"], vals["noobject_scale"], vals["object_scale"], vals["thresh"], vals["seen"], vals["device"])
 
-            loss_x = self.coord_scale * nn.MSELoss(size_average=False)(scale_split_output[:,:,0]*coord_mask, tx*coord_mask)/2.0
-            loss_y = self.coord_scale * nn.MSELoss(size_average=False)(scale_split_output[:,:,1]*coord_mask, ty*coord_mask)/2.0
-            loss_w = self.coord_scale * nn.MSELoss(size_average=False)(scale_split_output[:,:,2]*coord_mask, tw*coord_mask)/2.0
-            loss_h = self.coord_scale * nn.MSELoss(size_average=False)(scale_split_output[:,:,3]*coord_mask, th*coord_mask)/2.0
-            loss_conf = nn.MSELoss(size_average=False)(conf*conf_mask, tconf*conf_mask)/2.0
-            loss_cls = self.class_scale * nn.BCELoss(size_average=False)(cls, tcls)   # Todo
+            loss_x = self.coord_scale * nn.MSELoss(size_average=False)(
+                scale_split_output[:, :, 0]*coord_mask, tx*coord_mask)/2.0
+            loss_y = self.coord_scale * nn.MSELoss(size_average=False)(
+                scale_split_output[:, :, 1]*coord_mask, ty*coord_mask)/2.0
+            loss_w = self.coord_scale * nn.MSELoss(size_average=False)(
+                scale_split_output[:, :, 2]*coord_mask, tw*coord_mask)/2.0
+            loss_h = self.coord_scale * nn.MSELoss(size_average=False)(
+                scale_split_output[:, :, 3]*coord_mask, th*coord_mask)/2.0
+            loss_conf = nn.MSELoss(size_average=False)(
+                conf*conf_mask, tconf*conf_mask)/2.0
+            loss_cls = self.class_scale * \
+                nn.BCELoss(size_average=False)(cls, tcls)   # Todo
             loss = loss_x + loss_y + loss_w + loss_h + loss_conf + loss_cls
-            print("loss_x {} loss_y {} loss_w {} loss_h {} loss_conf {} loss_cls {}".format(loss_x , loss_y ,loss_w , loss_h, loss_conf ,loss_cls))
+            print("loss_x {} loss_y {} loss_w {} loss_h {} loss_conf {} loss_cls {}".format(
+                loss_x, loss_y, loss_w, loss_h, loss_conf, loss_cls))
             self.losses.append(loss)
         return sum(self.losses)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     from darknet import Darknet
     from detect import get_test_input
-    
+
     parser = argparse.ArgumentParser(description='YOLO v3 Detection Module')
-   
-    parser.add_argument("--cfg", dest = 'cfg', help = 
-                        "config file",
-                        default = "cfg/yolov3.cfg", type = str)
-    parser.add_argument("--reso", dest = 'reso', help = 
-                        "Input resolution of the network. Increase to increase accuracy. Decrease to increase speed",
-                        default = "416", type = str)
-    parser.add_argument("--tiny", action="store_true", help="Using yolov3-tiny")
-    parser.add_argument("--scales", dest = "scales", help = "Scales to use for detection",
-                        default = "1,2,3", type = str)                        
+
+    parser.add_argument("--cfg", dest='cfg', help="config file",
+                        default="cfg/yolov3.cfg", type=str)
+    parser.add_argument("--reso", dest='reso', help="Input resolution of the network. Increase to increase accuracy. Decrease to increase speed",
+                        default="416", type=str)
+    parser.add_argument("--tiny", action="store_true",
+                        help="Using yolov3-tiny")
+    parser.add_argument("--scales", dest="scales", help="Scales to use for detection",
+                        default="1,2,3", type=str)
     args = parser.parse_args()
     print(args)
-    
+
     CUDA = torch.cuda.is_available()
     device = torch.device("cuda" if CUDA else "cpu")
 
     scales = args.scales
     scales = [int(x) for x in scales.split(',')]
-    
+
     scales_indices = []
     args.reso = int(args.reso)
     strides = [8, 16, 32]
     resos = [args.reso//x for x in strides]
     num_boxes = sum([3*(x**2) for x in resos])
-    
+
     for scale in scales:
-        li = list(range((scale - 1)* num_boxes // 3, scale * num_boxes // 3))
+        li = list(range((scale - 1) * num_boxes // 3, scale * num_boxes // 3))
         scales_indices.extend(li)
 
     if CUDA:
         model = Darknet(args.cfg)
         model.net_info["height"] = args.reso
         inp_dim = int(model.net_info["height"])
-        assert inp_dim % 32 == 0 
+        assert inp_dim % 32 == 0
         assert inp_dim > 32
         model.cuda()
         model.eval()
         _output = model(get_test_input(inp_dim, CUDA), CUDA)
         print(model.blocks)
         #_output = convert2cpu(_output)
-    else :
+    else:
         inp_dim = args.reso
-        _output = torch.FloatTensor(np.ones((1,num_boxes,85)))
+        _output = torch.FloatTensor(np.ones((1, num_boxes, 85)))
 
-    _test_target = np.zeros((1,5*50))
-    _test_target[:,0] = 1
-    _test_target[:,1:5] = 0.2
-    _test_target[:,5] = 5
-    _test_target[:,6:10] = 0.1
+    _test_target = np.zeros((1, 5*50))
+    _test_target[:, 0] = 1
+    _test_target[:, 1:5] = 0.2
+    _test_target[:, 5] = 5
+    _test_target[:, 6:10] = 0.1
     test_target = torch.FloatTensor(_test_target).to(device)
-    anchors = [116,90,  156,198,  373,326]
+    anchors = [116, 90,  156, 198,  373, 326]
     #loss = V3Loss(num_classes=10, anchors=anchors, num_anchors=3)
     #lossval = loss(test_output,test_target)
 
     # use prediction when stride 32
-    nH=args.reso//32
-    nW=args.reso//32
-    nA= len(anchors) // 2
-    nC=80
-    
+    nH = args.reso//32
+    nW = args.reso//32
+    nA = len(anchors) // 2
+    nC = 80
+
     if False:
-        test_output = _output[:,0:nH*nW*nA,0:4]
+        test_output = _output[:, 0:nH*nW*nA, 0:4]
         vals = {
-            "output":test_output,
-            "target":test_target,
-            "anchors":anchors,
-            "nA":nA,
-            "nC":nC,
-            "nH":nH,
-            "nW":nW,
-            "noobject_scale":5,"object_scale":1,"thresh":0.6, "seen":False, "device": device
+            "output": test_output,
+            "target": test_target,
+            "anchors": anchors,
+            "nA": nA,
+            "nC": nC,
+            "nH": nH,
+            "nW": nW,
+            "noobject_scale": 5, "object_scale": 1, "thresh": 0.6, "seen": False, "device": device
         }
-        nGT, nCorrect, coord_mask, conf_mask, cls_mask, tx, ty, tw, th, tconf,tcls = build_targets(vals["output"],vals["target"],vals["anchors"],vals["nA"],vals["nC"],vals["nH"],vals["nW"],vals["noobject_scale"],vals["object_scale"],vals["thresh"],vals["seen"],vals["device"])
-        
+        nGT, nCorrect, coord_mask, conf_mask, cls_mask, tx, ty, tw, th, tconf, tcls = build_targets(
+            vals["output"], vals["target"], vals["anchors"], vals["nA"], vals["nC"], vals["nH"], vals["nW"], vals["noobject_scale"], vals["object_scale"], vals["thresh"], vals["seen"], vals["device"])
+
     if True:
         test_output = _output
-        v3loss = V3Loss(num_classes=80, anchors=anchors, num_anchors=3, resolution=args.reso, strides=strides, device=device)
-        lossval = v3loss(test_output,test_target)
+        v3loss = V3Loss(num_classes=80, anchors=anchors, num_anchors=3,
+                        resolution=args.reso, strides=strides, device=device)
+        lossval = v3loss(test_output, test_target)
         print(lossval)
